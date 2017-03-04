544992ee81157dd87a95e555826f28b395b0c70d
yadanilov19 <danilov.sergey.mo04@gmail.com>
2017-03-04 20:34:24 +0500

    msg

--- parralels/parralels/parralels.cpp
+++ parralels/parralels/parralels.cpp
@@ -13,16 +13,16 @@ void first() {
 	printf("hello world\n");
 }
 
-void secondHelper(char* str, TablePrinter *tp, function<void(int,int)> a) {
+void secondHelper(char* str, TablePrinter *tp, function<void(int, int)> a) {
 	double timeStatic = omp_get_wtime(),
 		timeDynamic,
 		timeGuided,
 		timeRuntime;
-	int n = 10;
+	int n = 1000;
 #pragma omp parallel  for schedule(static) firstprivate(n)
 	for (int i = 0; i < n; i++)
 	{
-		a(i,n);
+		a(i, n);
 	}
 
 	timeStatic = omp_get_wtime() - timeStatic;
@@ -30,14 +30,14 @@ void secondHelper(char* str, TablePrinter *tp, function<void(int,int)> a) {
 #pragma omp parallel  for schedule(dynamic) firstprivate(n)
 	for (int i = 0; i < n; i++)
 	{
-		a(i,n);
+		a(i, n);
 	}
 	timeDynamic = omp_get_wtime() - timeDynamic;
 	timeGuided = omp_get_wtime();
 #pragma omp parallel  for schedule(guided) firstprivate(n)
 	for (int i = 0; i < n; i++)
 	{
-		a(i,n);
+		a(i, n);
 	}
 	timeGuided = omp_get_wtime() - timeGuided;
 
@@ -45,7 +45,7 @@ void secondHelper(char* str, TablePrinter *tp, function<void(int,int)> a) {
 #pragma omp parallel  for schedule(runtime) firstprivate(n)
 	for (int i = 0; i < n; i++)
 	{
-		a(i,n);
+		a(i, n);
 	}
 	timeRuntime = omp_get_wtime() - timeRuntime;
 
@@ -65,20 +65,16 @@ void second() {
 	tp.PrintHeader();
 
 	secondHelper("Iterations with Normal Distrib", &tp, [](int i, int n) {
-		Sleep(1);
+		
 	});
-	
+
 	secondHelper("Iterations with i % 2 == 0 ", &tp, [](int i, int n) {
 		if (i % 2 == 0)
-			Sleep(100);
-		else
-			Sleep(10);
+			Sleep(1);
 	});
 	secondHelper("Iterations with i < n / 2 ", &tp, [](int i, int n) {
 		if (i <= n / 2)
-			Sleep(100);
-		else
-			Sleep(10);
+			Sleep(1);
 	});
 	tp.PrintFooter();
 }
@@ -96,8 +92,8 @@ void third() {
 	tp.AddColumn("time", 10);
 	tp.PrintHeader();
 
-	const int n = 500; 
-	int A[n], 
+	const int n = 500;
+	int A[n],
 		sumCriticalSetion = 0,
 		sumReduction = 0;
 	double timeCritSec = 0,
@@ -112,7 +108,7 @@ void third() {
 #pragma omp critical
 		{
 			sumCriticalSetion += A[i];
-		} 
+		}
 	}
 	timeCritSec = omp_get_wtime() - timeCritSec;
 	tp << "Critical Section" << timeCritSec;
@@ -130,32 +126,82 @@ void third() {
 }
 //
 void fourth() {
-#pragma omp parallel num_threads(10)
+#pragma omp parallel num_threads(3)
 	{
-		float a = rand();
-		if (a < 0.4)
-		{
-			Sleep(100);
-		}
-		else
-		{
-			Sleep(10);
-		}
-
+		printf("first\n");
 #pragma omp barrier
-		int threadNum = omp_get_thread_num();
-		printf("%f num thread %i \n", a, threadNum);
+		printf("second\n");
 	}
 }
 //
+
+
+const long n = 10000;
+int A[n][n];
+void fithInit() {
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			A[i][j] = 1;
+		}
+	}
+}
+void printTable(double time) {
+	printf("%f timeElapsed", time);
+	TablePrinter tp(&cout);
+	for (int i = 0; i < 10; i++)
+	{
+		tp.AddColumn("#", 10);
+	}
+	tp.PrintHeader();
+	for (long i = n - 10; i < n; i++)
+	{
+		for (long j = n - 10; j < n; j++)
+		{
+			tp << A[i][j];
+		}
+	}
+	tp.PrintFooter();
+}
 void fifth() {
-	//???
+	fithInit();
+	double timeParallel, timeSequance;
+	omp_set_num_threads(4);
+	//parallel
+	timeParallel = omp_get_wtime();
+	for (long numDiagonal = 4; numDiagonal < n * 2 - 1 ; numDiagonal++)
+	{
+		long j = max(2, numDiagonal - n + 1);
+#pragma omp parallel for firstprivate(n)
+		for (; j <= min(numDiagonal - 2, n - 1); j++)
+		{
+			A[j][numDiagonal - j] = A[j - 2][numDiagonal - j] + A[j][numDiagonal - j - 2];
+		}
+	}
+	timeParallel = omp_get_wtime() - timeParallel;
+	printTable(timeParallel);
+	
+	
+	fithInit();
+	timeSequance = omp_get_wtime();
+	//seq
+	for (long i = 2; i < n; i++)
+	{
+		for (long j = 2; j < n; j++)
+		{
+			A[i][j] = A[i - 2][j] + A[i][j - 2];
+		}
+	}
+	timeSequance = omp_get_wtime() - timeSequance;
+	printTable(timeSequance);
+
 }
 //
 int main()
 {
 	first();
-	second();
+	//second();
 	third();
 	fourth();
 	fifth();
